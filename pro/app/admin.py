8eb256091c233b0f1ee754fc64bf9f2859f9905e
Djangodev2010 <gaminglz507@gmail.com>
2026-07-05 21:20:25 +0530

    Updated Models

--- pro/app/admin.py
+++ pro/app/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import Valorant, ClashOfClan, GamerProfile, RecruiterProfile
+from .models import Valorant, ClashOfClan, GamerProfile, HostProfile
 
 # Register your models here.
 
 admin.site.register(Valorant)
 admin.site.register(ClashOfClan)
 admin.site.register(GamerProfile)
-admin.site.register(RecruiterProfile)
+admin.site.register(HostProfile)
