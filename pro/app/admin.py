05a869966b09106cd58e7aba20797138243d9eaa
Djangodev2010 <gaminglz507@gmail.com>
2026-07-05 15:05:57 +0530

    upd: added profile levels and event_credits for the recruiter

--- pro/app/admin.py
+++ pro/app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Valorant, ClashOfClan, GamerProfile
+from .models import Valorant, ClashOfClan, GamerProfile, RecruiterProfile
 
 # Register your models here.
 
 admin.site.register(Valorant)
 admin.site.register(ClashOfClan)
 admin.site.register(GamerProfile)
+admin.site.register(RecruiterProfile)
